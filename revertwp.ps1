ee7f23a8b771ea453f8ff55a057b21b56d599af0
Micaela Cerilla <64721005+Mica56@users.noreply.github.com>
2022-09-28 09:20:14 +0100

    Add files via upload

---
+++ revertwp.ps1
@@ -0,0 +1,22 @@
+﻿#T1491.001
+$updateWallpapercode = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{
+
+    public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+        }
+    }
+} 
+'@
+if (Test-Path -Path $env:TEMP\old.png -PathType Leaf) {
+     $orgImg = Get-Content -Path "$env:TEMP\old.png"
+     add-type $updateWallpapercode 
+     [Win32.Wallpaper]::SetWallpaper($orgImg)
+}
+Remove-Item "$env:TEMP\old.png" -ErrorAction Ignore
+Remove-Item "$env:TEMP\new.png" -ErrorAction Ignore
