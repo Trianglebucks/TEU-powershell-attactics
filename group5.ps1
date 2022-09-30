7582f9f27c644fedf2132677c8f23f7954fba08d
Micaela Cerilla <64721005+Mica56@users.noreply.github.com>
2022-09-30 06:23:06 +0100

    Add files via upload

---
+++ group5.ps1
@@ -0,0 +1,7 @@
+﻿# Windows Screen Capture (CopyFromScreen)
+Add-Type -AssemblyName System.Windows.Forms
+$screen = [Windows.Forms.SystemInformation]::VirtualScreen
+$bitmap = New-Object Drawing.Bitmap $screen.Width, $screen.Height
+$graphic = [Drawing.Graphics]::FromImage($bitmap)
+$graphic.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $bitmap.Size)
+$bitmap.Save("C:\Users\studentadmin\Desktop\vs.png")
